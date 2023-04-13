871e6be058a6b7e4fe4a9a47ebe4a80a2cc2681a
Gustavo <ghmpessa@gmail.com>
2023-04-13 20:12:10 -0300

    feat: add LoginModal component composition

---
+++ app/components/login-modal/index.tsx
@@ -0,0 +1,147 @@
+'use client'
+
+import { useCallback, useState } from 'react'
+import axios from 'axios'
+import { AiFillGithub } from 'react-icons/ai'
+import { FcGoogle } from 'react-icons/fc'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+import useLoginModal from '@/app/hooks/useLoginModal'
+import useRegisterModal from '@/app/hooks/useRegisterModal'
+import Modal from '../modal-template'
+import Heading from '../heading'
+import Input from '../input'
+import { toast } from 'react-hot-toast'
+import Button from '../button'
+import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+
+const LoginModal = () => {
+  const registerModal = useRegisterModal()
+  const loginModal = useLoginModal()
+  const [isLoading, setIsLoading] = useState(false)
+
+  const router = useRouter()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  const onSubmit: SubmitHandler<FieldValues> = async data => {
+    setIsLoading(true)
+
+    try {
+      console.log(data)
+      const res = await signIn('credentials', {
+        ...data,
+        redirect: false,
+      })
+      setIsLoading(false)
+
+      if (res?.ok) {
+        toast.success('Logged in')
+        router.refresh()
+        loginModal.onClose()
+      }
+
+      if (res?.error) {
+        toast.error(res.error)
+      }
+    } catch (error) {
+      console.warn(error)
+      toast.error('Something went wrong.')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const bodyContent = (
+    <div
+      className="
+        flex
+        flex-col
+        gap-4
+      "
+    >
+      <Heading title="Welcome back" subtitle="Login to your account!" />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        type="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  )
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+        outline
+      />
+      <Button
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => {}}
+        outline
+      />
+      <div
+        className="
+          text-neutral-500
+          text-center
+          mt-4
+          font-light
+        "
+      >
+        <div className="justify-center flex flex-row items-center gap-2">
+          <div>Already have an account?</div>
+          <div
+            onClick={loginModal.onClose}
+            className="
+              text-neutral-800
+              cursor-pointer
+              hover:underline
+            "
+          >
+            Log in
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={loginModal.isOpen}
+      title="Login"
+      actionLabel="Continue"
+      onClose={loginModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
+    />
+  )
+}
+
+export default LoginModal
