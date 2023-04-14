dd92f973c62f67e857fe6c8ce4bb61a3fa7b40c8
Gustavo <ghmpessa@gmail.com>
2023-04-14 19:21:40 -0300

    refactor: add buildings list to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,48 @@
-export default function Home() {
-  return <div className="text-rose-500 text-2xl">Hello</div>
+import ClientOnly from '@/app/components/client-only'
+import Container from '@/app/components/container'
+import EmptyState from '@/app/components/empty-state'
+import getListings from '@/app/actions/get-listings'
+import ListingCard from '@/app/components/listing-card'
+import getCurrentUser from './actions/get-current-user'
+
+export default async function Home() {
+  const currentUser = await getCurrentUser()
+
+  const listings = await getListings()
+  const isEmpty = listings.length === 0
+
+  if (isEmpty)
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    )
+
+  return (
+    <ClientOnly>
+      <Container>
+        <div
+          className="
+          pt-24
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md-grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+        >
+          {listings.map(listing => (
+            <ListingCard
+              key={listing.id}
+              data={listing}
+              currentUser={currentUser}
+            />
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
+  )
 }
