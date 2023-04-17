22cab6f2aa4a9687825073f5bed964d930d41d56
Gustavo <ghmpessa@gmail.com>
2023-04-17 17:19:48 -0300

    feat: add FavoritePage view

---
+++ app/favorites/page.tsx
@@ -0,0 +1,29 @@
+import EmptyState from '../components/empty-state'
+import ClientOnly from '../components/client-only'
+
+import getCurrentUser from '../actions/get-current-user'
+import getFavoriteListings from '../actions/get-favorite-listings'
+import FavoritesClient from './favorites-client'
+
+const FavoritesPage = async () => {
+  const listings = await getFavoriteListings()
+  const currentUser = await getCurrentUser()
+
+  if (listings.length === 0)
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No favorites found"
+          subtitle="Looks like you have no favorite listings."
+        />
+      </ClientOnly>
+    )
+
+  return (
+    <ClientOnly>
+      <FavoritesClient listings={listings} currentUser={currentUser} />
+    </ClientOnly>
+  )
+}
+
+export default FavoritesPage
